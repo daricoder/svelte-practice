54e7d79d055d41300ed474115138f22fae27f937
Ruben Suarez B <daricoder@gmail.com>
2021-09-30 19:40:33 -0500

    Terminando Formulario aunque necesito comprender mas la forma reactiva

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ReacTividad from './components/Reactividad.svelte'
+	import Nested from './components/exports.svelte'
+	import Bindings from './components/Bindings.svelte'
+
+</script>
+
+<ReacTividad/>
+<!-- no importa el nombre con el importa ya que es lo importa por default segun la documentacion -->
+<Nested nombre="Ruben" cualquier/>
+<Bindings/>

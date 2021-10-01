a6fd63505665e76ad48c94d1d4b4a47d22708fc7
Ruben Suarez B <daricoder@gmail.com>
2021-09-30 19:44:22 -0500

    codigo inicial limpio

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,7 @@
 <script>
-	import ReacTividad from './components/Reactividad.svelte'
-	import Nested from './components/exports.svelte'
-	import Bindings from './components/Bindings.svelte'
+
+let saludo = "Hola mundo"
 
 </script>
 
-<ReacTividad/>
-<!-- no importa el nombre con el importa ya que es lo importa por default segun la documentacion -->
-<Nested nombre="Ruben" cualquier/>
-<Bindings/>
+<h1>{saludo}</h1>

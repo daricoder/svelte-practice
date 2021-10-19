e306774d6015cca3f5afe3730965df5f88e0fad0
Ruben Suarez B <daricoder@gmail.com>
2021-10-19 13:05:13 -0500

    aniadiendo rutas

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,25 @@
 <script>
+import {Router,Route,Link} from 'svelte-navigator'
+import About from './About.svelte'
+
 
 let saludo = "Hola mundo"
 
 </script>
 
-<h1>{saludo}</h1>
+
+<Router>
+
+    <Link to="/about">
+    about
+    </Link>
+
+    <Route path="/about" component={About}>
+    </Route>
+
+    <Route path="/">
+        <h1>hello world</h1>
+    </Route>
+
+    
+</Router>
